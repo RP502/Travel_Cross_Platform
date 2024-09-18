b8adfe53051e503c32daad3c440128c01be24bde
hanhl3 <hanhle.dev16@gmail.com>
2024-09-18 10:50:36 +0700

    login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,15 @@
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
 export default function RootLayout() {
- 
-
-
   return (
-    
-      <Stack>
-         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        
-        
-      </Stack>
-    
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+    </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,24 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useFonts } from 'expo-font'
+import Login from './(auth)/login'
 
-const index = () => {
+
+
+const index: React.FC = () => {
+
+  useFonts({
+    'Poppins': require('@/assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Bold': require('@/assets/fonts//Poppins-Bold.ttf'),
+    'Poppins-ExtraBold': require('@/assets/fonts//Poppins-ExtraBold.ttf'),
+    'Poppins-Medium': require('@/assets/fonts//Poppins-Medium.ttf'),
+    'Poppins-Light': require('@/assets/fonts//Poppins-Light.ttf'),
+    'Poppins-Thin': require('@/assets/fonts//Poppins-Thin.ttf'),
+  })
+
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={{ flex: 1 }}>
+      <Login />
     </View>
   )
 }
